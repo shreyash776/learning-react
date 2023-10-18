2ecd00b1b760568f8d86b5c30943aa532f390a11
shreyash <shreyashchaurasia77@gmail.com>
2023-10-18 20:02:48 +0530

    created a head component

---
+++ my-react-vite-app/src/head.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+ function Head () {
+    return <h1>HELLO I AM SHREYASH </h1>;
+ }
+  
+ export default Head ;
--- my-react-vite-app/src/main.jsx
+++ my-react-vite-app/src/main.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Head from "./head.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- <div><h1>Hello world</h1></div>
+ <div>
+   < Head />
+ </div>
 )
  
