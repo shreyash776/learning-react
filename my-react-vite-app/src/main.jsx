695b77047299673e80cf9319c0890ba3803e26cf
shreyash <shreyashchaurasia77@gmail.com>
2023-10-18 08:47:23 +0530

    created a webpage of ListItems

--- my-react-vite-app/src/main.jsx
+++ my-react-vite-app/src/main.jsx
@@ -4,6 +4,15 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- <div><h1>Hello world</h1></div>
+ <div>
+    <h1>Students in react Domain </h1>
+    <ul>
+        <li>Shreyash</li>
+        <li>Akash</li>
+        <li>Som</li>
+        <li>Prashant</li>
+        <li>Anshika</li>
+    </ul>
+ </div>
 )
  
