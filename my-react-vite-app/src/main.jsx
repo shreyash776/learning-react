09d9ea8a4c1eb254f35f3c8f729e4f05e1c972ff
shreyash <shreyashchaurasia77@gmail.com>
2023-10-18 19:47:27 +0530

    created indicator

--- my-react-vite-app/src/main.jsx
+++ my-react-vite-app/src/main.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+ 
+  const date = new Date();
+  const time = date.getHours();
+  let greeting;
+  const customStyle ={
+    color:""
+  }
 
+ if(time < 12){
+    greeting ="Good Morning";
+    customStyle.color="yellow";
+ }
+ else if(time<18){
+    greeting="Good Afternoon";
+    custonStyle.color="green";
+
+ }
+ else{
+    greeting="Good Night";
+    customStyle.color="red";
+ }
 ReactDOM.createRoot(document.getElementById('root')).render(
- <div><h1>Hello world</h1></div>
+ <div>
+ <h1 style={customStyle}   >{greeting}</h1>
+ </div>
 )
  
